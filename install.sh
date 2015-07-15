ab614de1c5563941c9e0cf04cd06156dee2b8149
tientun <tientunit@gmail.com>
2015-07-15 10:33:36 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
+rm -rf $HOME/.gittool
 if [ ! -d "$HOME/.gittool" ]; then
     echo "Installing gittool for the first time"
     git clone https://github.com/tientun/.git_branch_tool.git "$HOME/.gittool"
     cd "$HOME/.gittool"
     export NAME="$1"
     LC_ALL=C find ~/.gittool/gitcfg -type f -exec sed -i '' "s/{USERNAME}/$NAME/g" {} \;
-    ln -s ~/.gittool/gitcfg ~/.bla
+    ln -s ~/.gittool/gitcfg ~/.gitconfig
 else
     echo "gittool is already installed"
 fi
