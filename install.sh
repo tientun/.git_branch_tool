e319bb21d85622f22fd0a97550d8952e7fa268c8
tientun <tientunit@gmail.com>
2015-07-15 09:51:15 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-ln -s ~/.gittool/gitcfg ~/.bla
+#!/bin/sh
+
+if [ ! -d "$HOME/.gittool” ]; then
+    echo "Installing gittool for the first time"
+    git clone https://github.com/tientun/git_branch_tool.git "$HOME/.gittool"
+    cd "$HOME/.yadr"
+    export NAME="$1"
+    ln -s ~/.gittool/gitcfg ~/.bla
+else
+    echo “gittool is already installed"
+fi
